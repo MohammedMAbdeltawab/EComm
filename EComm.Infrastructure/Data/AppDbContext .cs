2a6ccf22e34eb5fa5fe173d5a441e427eba1e365
Mohammed <MohammedM.Abdeltawab@gmail.com>
2025-10-19 01:54:49 +0300

    CorrectAprroch

--- EComm.Infrastructure/Data/AppDbContext .cs
+++ EComm.Infrastructure/Data/AppDbContext .cs
@@ -19,11 +19,5 @@ public class AppDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        // Seed sample data
-        modelBuilder.Entity<WeatherForecast>().HasData(
-            new WeatherForecast { Id = 1, Date = DateTime.Now, TemperatureC = 25, Summary = "Sunny" },
-            new WeatherForecast { Id = 2, Date = DateTime.Now.AddDays(1), TemperatureC = 18, Summary = "Cloudy" },
-            new WeatherForecast { Id = 3, Date = DateTime.Now.AddDays(2), TemperatureC = 12, Summary = "Rainy" }
-        );
     }
 }
