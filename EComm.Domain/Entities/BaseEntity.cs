9108e5d923c2957161fe6b9bdf42132a38a17ba2
Mohammed <MohammedM.Abdeltawab@gmail.com>
2025-10-19 01:59:16 +0300

    Structured And Run

--- EComm.Domain/Entities/BaseEntity.cs
+++ EComm.Domain/Entities/BaseEntity.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EComm.Domain.Entities;
+﻿namespace EComm.Domain.Entities;
 public abstract class BaseEntity
 {
-    public Guid Id { get; set; } 
+    public int Id { get; set; } 
     public bool IsDeleted { get; set; }
     public DateTime CreationTime { get; set; }
 }
--- EComm.Domain/Entities/WeatherForecast.cs
+++ EComm.Domain/Entities/WeatherForecast.cs
@@ -3,5 +3,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EComm.Domain.Entities
 {
-   
+    public class WeatherForecast : BaseEntity
+    {
+        public DateTime Date { get; set; }
+        public int TemperatureC { get; set; }
+        public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
 }
