0df3873a693421c1228935fb49fde6ae7126f226
Mohammed <MohammedM.Abdeltawab@gmail.com>
2025-10-19 01:31:48 +0300

    Initialize And Configure The Project

---
+++ EComm.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EComm.Domain.Entities;
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; } 
+    public bool IsDeleted { get; set; }
+    public DateTime CreationTime { get; set; }
+}
